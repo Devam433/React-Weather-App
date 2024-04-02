5f5cc48de85e54206ea9f25f05a2b5dd32d1761e
Devam <devam433@gmail.com>
2024-04-02 15:48:52 +0530

    Add file to integrate AOI

---
+++ src/API/WeatherVisualCrossing.jsx
@@ -0,0 +1,34 @@
+import axios from "axios"
+//26.655041,92.772453 tezpur coords
+const APIKEY='YNWMKP6R55KU3E4VNFQ5M38AY';
+
+async function getGeolocation() {
+    try{
+        const position = new Promise((resolve,reject) => {
+            navigator.geolocation.getCurrentPosition(resolve,reject);
+        })
+        return position;
+    } catch(err) {
+        console.log('Failed getting geolocation',err);
+    }
+}
+async function getCurrentWeatherData() {
+    try {
+        const position = await getGeolocation();
+        const { latitude,longitude } = position.coords;
+        const openWeatherApi = axios.create({
+            baseURL:`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${latitude},${longitude}`,
+            params: {
+                aggregateHours:'24',
+                key:APIKEY,
+                contentType: 'json',
+                unitGroup: 'metric'
+            }
+        })
+        return openWeatherApi;
+    } catch(err){
+        console.log(err);
+    }
+}
+
+export default getCurrentWeatherData;

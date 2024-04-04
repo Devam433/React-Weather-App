80984d51ad2fc287892ecb26e06e4f9b749fb6c2
Devam <devam433@gmail.com>
2024-04-04 16:04:19 +0530

    Add file

---
+++ src/components/WeatherIcon.js
@@ -0,0 +1,9 @@
+import sunPng from "../assets/sun.png"
+
+const icon={
+    "Clear": sunPng,
+    "Partially cloudy": "partly-cloudy.png",
+    "Cloudy": "cloudy.png",
+    "Rain": "rain.png",
+}
+export default icon;
